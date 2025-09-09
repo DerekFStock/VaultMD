de3630823be9339bbd90c0f6ac41f1d95270000f
Derek Stock <derekstock@gmail.com>
2025-09-09 14:34:04 -0700

    First commit

---
+++ VaultMDPrototype/VaultMDPrototypeApp.swift
@@ -0,0 +1,30 @@
+//
+//  VaultMDPrototypeApp.swift
+//  VaultMDPrototype
+//
+//  Created by Derek Stock on 9/1/25.
+//
+
+import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+
+@main
+struct VaultMDPrototypeApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
